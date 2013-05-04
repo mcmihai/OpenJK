fa7d1070f3d4f8caa95c10116daf31f49694a7e0
Mihai Melinescu <mihaimel@gmail.com>
2013-05-04 17:57:57 +0300

    added steam install path

--- CreateWindowsVS10Projects.bat
+++ CreateWindowsVS10Projects.bat
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-cmake -G "Visual Studio 10" -D CMAKE_INSTALL_PREFIX=../install ..
+cmake -G "Visual Studio 10" -D CMAKE_INSTALL_PREFIX="D:\Steam\steamapps\common\Jedi Academy\GameData" ..
 pause
